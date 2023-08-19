60616dfbb67777345ff37e40b174bbb92f132ae6
rahulbatra-19 <batrarahul409@example.com>
2023-08-19 16:03:16 +0530

    Lifecyccle of dispacthing an action

--- src/index.js
+++ src/index.js
@@ -2,6 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./componets/App";
+import { legacy_createStore as createStore } from "redux";
+import movies from "./reducers";
+
+const store = createStore(movies);
+console.log("store", store);
+console.log("Before  State", store.getState());
+
+store.dispatch({
+  type: "ADD_MOVIES",
+  movies: [{ name: "Superman" }],
+});
+
+console.log("after  State", store.getState());
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
